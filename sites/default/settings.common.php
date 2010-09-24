c83316d703272a2191f2577e3a4714f9d7452da7
Brenda Wallace <shiny@cpan.org>
2010-09-24 12:28:01 +1200

    simpletest needs this

--- sites/default/settings.common.php
+++ sites/default/settings.common.php
@@ -2,3 +2,7 @@
 
 // This file is included from settings.php in both dev and dh-make-drupal deployed sites
 // Put any settings that are shared between staging, prod and dev in here
+$GLOBALS['simpletest_installed'] = TRUE;
+if (preg_match("/^simpletest\d+$/", $_SERVER['HTTP_USER_AGENT'])) {
+  $db_prefix = $_SERVER['HTTP_USER_AGENT'];
+}
